5d22e3ee2c54d213621ff7e3cc5de6781dae931a
alylopez02 <alylopez0217@gmail.com>
2022-07-16 17:39:41 -0400

    finished routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Application } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     // Get all users
@@ -24,15 +24,15 @@ module.exports = {
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
-    // Delete a user and associated apps
+    // Delete a user and associated thoughts
     deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: 'No user with that ID' })
-                    : Application.deleteMany({ _id: { $in: user.applications } })
+                    : Thought.deleteMany({ _id: { $in: user.thought } }) // MAYBE INCORRECT PLZ DOUBLE CHECK (THOUGHT REPLACED APPLICATION HERE)
             )
-            .then(() => res.json({ message: 'User and associated apps deleted!' }))
+            .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
 
