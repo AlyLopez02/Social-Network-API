ce5968693ae9973ce7f9a1958812b100be751b14
alylopez02 <alylopez0217@gmail.com>
2022-07-16 20:38:46 -0400

    possibly finished files in utils folder

---
+++ utils/data.js
@@ -0,0 +1,123 @@
+const username = [
+'catlover9',
+'doglover7',
+'nerdygirl144',
+'hulkfan77',
+'batmanhater0',
+'dinofinder1',
+'horselover44',
+'mickeymouse08',
+// 'greenlantern67',
+// 'wonderwoman88',
+// 'minniemouse80',
+// 'wordmaster99',
+// 'amazingauthor2',
+// 'cameracadet66',
+// 'gothamknight44',
+// 'lewislane03',
+// 'supergirl7',
+// 'greenarrow46',
+// 'flashzoomzoom',
+// 'hawkgirl',
+// 'lavagirl1997'
+];
+
+const email =[
+'c.gerarld@email.com',
+'d.smith@email.com',
+'j.johnson@email.org',
+'a.adams@email.com',
+'b_barnes@email.com',
+'c-clementine@email.org',
+'d-derrick@email.com',
+'e.evans@email.com',
+// 'f_fonds@email.org',
+// 'g-garrick@email.org',
+// 'h-harrolds@email.com',
+// 'i.iverson@email.org',
+// 'j-joshua@email.com',
+// 'k-karmen@email.com',
+// 'l_linus@email.com',
+// 'm-matthews@email.com',
+// 'n.nichols@email.org',
+// 'o.otterson@email.org',
+// 'p.potter@email.org',
+// 'q.quincy@email.org',
+// 'r.robinson@email.org'
+];
+
+const thoughtText =[
+'Fish are better than people!',
+'Batman is not a super hero.',
+'Mechanical pencils are better than regular ones.',
+'Staying home is better than going out.',
+'Video games are bad for kids.',
+'Video games are good for kids and are super fun!',
+'The books are better than the movies.',
+'Batman is an amazing super hero!',
+'Ice cream is best in the winter.',
+'Evelators are terrifying.',
+'The sunset is prettier than the sunrise.',
+'Dinos are super cool!',
+'Dinos are lame.',
+'Startbucks sucks.',
+'Starbucks is great!',
+'McDonalds is the best fast food.',
+'Burger King is the best fast food.',
+'Wendys is the best fast food.',
+'Coding is annoying.',
+'Coding is super fun!',
+'Batman would win against all the other superheroes.'
+];
+
+const reactionBody =[
+'I agree!',
+'No way!',
+'For sure',
+'Obviously',
+'You can not be serious...',
+'Disappointed in you...',
+'I am not mad, just disappointed',
+'Disagree',
+'Completely agree',
+'I know right?',
+'Amazing!',
+'Wow!',
+'Wow...'
+];
+
+
+// Get random array item
+const getRandomArrayItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const getRandomUsername = () =>
+    `${getRandomArrayItem(username)}`;
+
+const getRandomThought = () => 
+    `${getRandomArrayItem(thoughtText)}`;
+
+const getRandomEmail = () =>
+    `${getRandomArrayItem(email)}`;
+    
+const getRandomFriends = () => {
+    const friends = [];
+    for (let i = 0; i < int; i++) {
+      friends.push({
+        username: getRandomUsername(),
+      });
+    }
+    return results;
+};
+
+const getRandomReactions = () => {
+    const reactions = [];
+    for (let i = 0; i < int; i++) {
+      reactions.push({
+        reactionBody: getRandomArrayItem(reactionBody),
+        username: getRandomUsername()
+      });
+    }
+    return reactions;
+};
+
+module.exports = { getRandomUsername, getRandomThought, getRandomEmail, getRandomFriends, getRandomReactions };
---
+++ utils/seed.js
@@ -0,0 +1,65 @@
+const connection = require('../config/connection');
+const { Course, Student } = require('../models');
+const { getRandomUsername, getRandomThought, getRandomEmail, getRandomFriends, getRandomReactions } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+
+    // Drop existing users
+    await User.deleteMany({});
+
+    // Drop existing thoughts
+    await Thought.deleteMany({});
+
+    // Create empty array to hold the thoughts
+    const thoughts = [];
+
+    // Loop 20 times -- add thoughts to the thoughts array
+    for (let i = 0; i < 20; i++) {
+        // Get some random assignment objects using a helper function that we imported from ./data
+        const reactions = getRandomReactions(5);
+
+        const thoughtText = getRandomThought();
+        const username = getRandomUsername();
+
+        thoughts.push({
+            thoughtText,
+            username,
+            reactions
+        });
+    }
+
+    // Add students to the collection and await the results
+    await Thought.collection.insertMany(thoughts);
+
+
+    // Create empty array to hold users
+    const users = [];
+
+    // Loop 8 times
+    for (let i = 0; i < 8; i++) {
+        // Get some random assignment objects using a helper function that we imported from ./data
+        const friends = getRandomFriends(3);
+
+        const username = getRandomUsername();
+        const email = getRandomEmail();
+
+        users.push({
+            username,
+            email,
+            thoughts: [...thoughts],
+            friends
+        });
+    }
+
+
+    // Add courses to the collection and await the results
+    await User.collection.insertMany(users);
+
+    // Log out the seed data to indicate what should appear in the database
+    console.table(thoughts);
+    console.info('Seeding finished!');
+    process.exit(0);
+});
